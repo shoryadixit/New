f834cf41f27b7e6d70fe454d79342e6194254431
Shorya.dixit <shauryadt123@gmail.com>
2020-07-24 13:55:50 +0530

    Initial commit

---
+++ src/CommandlineDemo.java
@@ -0,0 +1,11 @@
+// printing command line arguments
+
+public class CommandlineDemo{
+		public static void main(String[] args){
+			System.out.println("length of args-->" + args.length);
+			System.out.println("Argument 1-->" + args[0]);
+			System.out.println("Argument 2-->" + args[1]);
+			System.out.println("Argument 3-->" + args[2]);
+}
+}
+	
